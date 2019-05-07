4bdd1c213ef84031e73eb71cba18e0ec1bd09e29
fluxcontinuum <jordan@fluxcontinuum.com>
2019-05-07 07:43:20 -0500

    Static build and env

---
+++ api/client/build/precache-manifest.278577c3c35ab2e01a4790d439567ed4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00373facfe28a9cce89148610e91bdc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e7deb44e76e977a1228",
+    "url": "/static/css/main.ebed6c00.chunk.css"
+  },
+  {
+    "revision": "71da1e07a2571ef286c5",
+    "url": "/static/js/2.7e249968.chunk.js"
+  },
+  {
+    "revision": "7e7deb44e76e977a1228",
+    "url": "/static/js/main.1bfa25e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
